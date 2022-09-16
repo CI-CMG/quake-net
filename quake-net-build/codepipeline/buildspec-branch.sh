1bd938950c790322ff615541ea60d0cec5e24181
Chris Slater <6004777+electricsam@users.noreply.github.com>
2022-09-16 13:54:05 -0600

    use dynamic git checkout

---
+++ quake-net-build/codepipeline/buildspec-branch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+github_pat="$1"
+github_username="$2"
+
+git checkout @git.branch@
+mvn -B build-helper:parse-version -Dgit.username=$github_username -Dgit.password=$github_pat -DbranchName='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}' -DdevelopmentVersion='${parsedVersion.majorVersion}.${parsedVersion.nextMinorVersion}.0-SNAPSHOT' release:branch
