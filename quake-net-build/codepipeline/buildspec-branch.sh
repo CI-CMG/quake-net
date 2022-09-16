1b12eb5d70fb88c9753bdd1567af21432205efc8
Chris Slater <6004777+electricsam@users.noreply.github.com>
2022-09-16 15:04:10 -0600

    fix branch scripts

--- quake-net-build/codepipeline/buildspec-branch.sh
+++ quake-net-build/codepipeline/buildspec-branch.sh
@@ -5,5 +5,5 @@ set -ex
 github_pat="$1"
 github_username="$2"
 
-git checkout @git.branch@
+git checkout @project.mainBranch@
 mvn -B build-helper:parse-version -Dgit.username=$github_username -Dgit.password=$github_pat -DskipTests -DbranchName='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}' -DdevelopmentVersion='${parsedVersion.majorVersion}.${parsedVersion.nextMinorVersion}.0-SNAPSHOT' release:branch
