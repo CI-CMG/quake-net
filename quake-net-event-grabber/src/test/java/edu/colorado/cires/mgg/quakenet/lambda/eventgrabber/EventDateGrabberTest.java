f6a8e98105a767597945fea424d3ee5f8ffba86c
peemin <peemin.chen@colorado.edu>
2024-05-16 09:05:30 -0600

    test

--- quake-net-event-grabber/src/test/java/edu/colorado/cires/mgg/quakenet/lambda/eventgrabber/EventDateGrabberTest.java
+++ quake-net-event-grabber/src/test/java/edu/colorado/cires/mgg/quakenet/lambda/eventgrabber/EventDateGrabberTest.java
@@ -32,6 +32,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 class EventDateGrabberTest {
 
   private final ObjectMapper objectMapper = ObjectMapperCreator.create();
