34df0d2c17e28190ad8858b8664c585924b994ae
peemin <peemin.chen@colorado.edu>
2025-07-24 10:56:07 -0600

    Github build

--- quake-net-build/codepipeline/buildspec-branch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-github_pat="$1"
-github_username="$2"
-
-git checkout @project.mainBranch@
-mvn -B build-helper:parse-version -Dgit.username=$github_username -Dgit.password=$github_pat -DskipTests -DbranchName='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}' -DdevelopmentVersion='${parsedVersion.majorVersion}.${parsedVersion.nextMinorVersion}.0-SNAPSHOT' release:branch
